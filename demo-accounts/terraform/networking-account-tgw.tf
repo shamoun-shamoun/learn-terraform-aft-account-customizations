4febecf9243e71ab18a01073bab26918dac4b5b1
Shamoun-Shamoun <r0793623@student.thomasmore.be>
2022-04-04 11:48:37 +0200

    tgw tf vpc attachemenet edited

--- demo-accounts/terraform/networking-account-tgw.tf
+++ demo-accounts/terraform/networking-account-tgw.tf
@@ -159,9 +159,9 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "shared-vpc-attach" {
   subnet_ids = [ aws_subnet.shared-attach-sub-1.id, aws_subnet.shared-attach-sub-2.id ]
   transit_gateway_id = aws_ec2_transit_gateway.shared-tgw.id
   vpc_id = aws_vpc.shared-vpc.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.rt-shared-domain.id
-  transit_gateway_default_route_table_association = true
-  transit_gateway_default_route_table_propagation = true
+  #transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.rt-shared-domain.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
 }
 
 
@@ -243,9 +243,14 @@ resource "aws_ram_resource_association" "shared-tgw-resource-association" {
   resource_share_arn = aws_ram_resource_share.share-tgw-resource.arn
 }
 
+output "orgID" {
+  value = data.aws_organizations_organization.organization.arn
+}
+
+data "aws_organizations_organization" "organization" {}
 #Associating the principal(AWS Organisation) with the RAM share
 resource "aws_ram_principal_association" "shared-tgw-principal-association" {
-  principal = "arn:aws:organizations::266981674444:organization/o-m38a60h090"
+  principal = "${data.aws_organizations_organization.organization.arn}"
   resource_share_arn = aws_ram_resource_share.share-tgw-resource.arn
 }
 
